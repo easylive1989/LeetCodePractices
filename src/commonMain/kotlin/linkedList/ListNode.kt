1defaef1e7f6095594670a529fa0c64789c95863
Paul <easylive1989@gmail.com>
2021-07-09 07:58:59 +0800

    add more challenge

---
+++ src/commonMain/kotlin/linkedList/ListNode.kt
@@ -0,0 +1,13 @@
+package linkedList
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class LinkedListUtils {
+    fun deleteNode(node: ListNode?) {
+        while(node.next != null) {
+
+        }
+    }
+}
